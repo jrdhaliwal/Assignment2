e041311485f1ad9e4a3c4e55d5eef9d1619c1aa4
jrdhaliwal <jrdhaliwal101@gmail.com>
2023-11-25 03:02:04 -0700

    Added changing views

--- starting-files/assign2.js
+++ starting-files/assign2.js
@@ -12,5 +12,73 @@ const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.p
    use built-in Live Preview.
 */
 document.addEventListener("DOMContentLoaded", function() {
+   const homeContainer = document.querySelector("#homeContainer");
+   const searchContainer = document.querySelector("#searchContainer");
+   const playlistContainer = document.querySelector("#playlistContainer")
+   const detailContainer = document.querySelector("#detailContainer")
+
+   const logo = document.querySelector("#logo");
+   const searchButton = document.querySelector("#searchButton");
+   const playlistButton = document.querySelector("#playlistButton");
+   const detailButton = document.querySelector("#detailButton");
+
+   logo.addEventListener("click", function(e) {
+      if(e.target.nodeName == "H1") {
+         showHideHome(e);
+      }
+   })
+
+   searchButton.addEventListener("click", function(e) {
+      if(e.target.nodeName == "BUTTON") {
+         showHideSearch(e);
+      }
+   })
+
+   playlistButton.addEventListener("click", function(e) {
+      if(e.target.nodeName == "BUTTON") {
+         showHidePlaylist(e);
+      }
+   })
+
+   detailButton.addEventListener("click", function(e) {
+      if(e.target.nodeName == "BUTTON") {
+         showHideDetails(e);
+      }
+   })
+
+   function showHideHome(event) {
+      if(homeContainer.className == "hide") {
+         searchContainer.className = "hide";
+         homeContainer.className = "show";
+         playlistContainer.className = "hide";
+         detailContainer.className = "hide";
+      }
+   }   
+
+   function showHidePlaylist(event) {
+      if(playlistContainer.className == "hide") {
+         searchContainer.className = "hide";
+         homeContainer.className = "hide";
+         playlistContainer.className = "show";
+         detailContainer.className = "hide";
+      }
+   }
+
+   function showHideDetails(event) {
+      if(detailContainer.className == "hide") {
+         searchContainer.className = "hide";
+         homeContainer.className = "hide";
+         playlistContainer.className = "hide";
+         detailContainer.className = "show";
+      }
+   }
    
+   function showHideSearch(event) {
+      if(searchContainer.className == "hide") {
+         searchContainer.className = "show";
+         homeContainer.className = "hide";
+         playlistContainer.className = "hide";
+         detailContainer.className = "hide";
+      }
+   }
 })
